7e314042d8bcb6004630da9d8221994759493914
windx-foobar <bezalkogoln1ycoder@gmail.com>
2024-05-28 02:34:53 +0300

    chore(scripts/inject-to-project): пути до проектов через конфигурационный файл

--- scripts/inject-to-project.js
+++ scripts/inject-to-project.js
@@ -2,31 +2,24 @@
 
 import { promises as fsp } from 'node:fs';
 import * as npath from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { read } from 'rc9';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 (async () => {
-  const projectPath = process.argv[2];
-  if (!projectPath) throw new Error('Нужно передать путь до проекта в который делаем силинк');
+  /** @type {{ injectToProject: { dir: string[] } }} */
+  const config = read(npath.resolve(__dirname, '.confrc'));
 
   const fromRootPath = (...args) => {
     const rootPath = new URL('../', import.meta.url).pathname;
     return npath.join(rootPath, ...args);
   };
 
-  const fromProjectPath = (...args) => {
-    const isNodeModulesEnd = /node_modules/.test(projectPath);
-
-    const paths = [projectPath, isNodeModulesEnd ? null : 'node_modules', ...args].filter(Boolean);
-
-    return npath.join(...paths);
-  };
-
   const pkg = await fsp
     .readFile(fromRootPath('package.json'))
     .then((buf) => JSON.parse(buf.toString()))
     .catch(() => ({ name: 'nuxt-primevue-bootstrap5' }));
-
-  const symbolicPath = fromProjectPath(pkg.name);
-  const symbolicDistPath = fromProjectPath(pkg.name, 'dist');
 
   const fileAndDirPaths = {
     'package.json': {
@@ -67,22 +60,35 @@ import * as npath from 'node:path';
     }
   };
 
-  await fsp.rm(symbolicPath, { recursive: true, force: true });
-  await fsp.mkdir(symbolicDistPath, { recursive: true });
+  for (const projectPath of config.injectToProject.dir) {
+    const fromProjectPath = (...args) => {
+      const isNodeModulesEnd = /node_modules/.test(projectPath);
 
-  await Promise.all(
-    Object.entries(fileAndDirPaths).map(async ([name, item]) => {
-      const symbPath = fromProjectPath(pkg.name, name);
+      const paths = [projectPath, isNodeModulesEnd ? null : 'node_modules', ...args].filter(Boolean);
 
-      if (item.content) {
-        await fsp.writeFile(symbPath, item.content);
-      } else {
-        await fsp[item.method](item.path, symbPath);
-      }
-    })
-  );
+      return npath.join(...paths);
+    };
 
-  console.log(`All contents is symlinked in "${symbolicPath}"`);
+    const symbolicPath = fromProjectPath(pkg.name);
+    const symbolicDistPath = fromProjectPath(pkg.name, 'dist');
+
+    await fsp.rm(symbolicPath, { recursive: true, force: true });
+    await fsp.mkdir(symbolicDistPath, { recursive: true });
+
+    await Promise.all(
+      Object.entries(fileAndDirPaths).map(async ([name, item]) => {
+        const symbPath = fromProjectPath(pkg.name, name);
+
+        if (item.content) {
+          await fsp.writeFile(symbPath, item.content);
+        } else {
+          await fsp[item.method](item.path, symbPath);
+        }
+      })
+    );
+
+    console.log(`All contents is symlinked in "${symbolicPath}"`);
+  }
 })()
   .then(() => {
     process.exit();
