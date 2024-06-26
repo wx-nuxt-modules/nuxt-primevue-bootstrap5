2a71633463d158fcfe4234a3449b7341702b010a
windx-foobar <bezalkogoln1ycoder@gmail.com>
2024-06-26 19:54:39 +0300

    chore(bootstrap5/message): create shared file and use it

--- src/runtime/presets/bootstrap5/message/index.ts
+++ src/runtime/presets/bootstrap5/message/index.ts
@@ -1,46 +1,22 @@
 import type { MessagePassThroughOptions } from 'primevue/message';
-import type { Icon } from 'primevue/icons';
 
-import CheckIcon from 'primevue/icons/check';
-import ExclamationTriangleIcon from 'primevue/icons/exclamationtriangle';
-import InfoCircleIcon from 'primevue/icons/infocircle';
-import TimesCircleIcon from 'primevue/icons/timescircle';
-
+import {
+  createRootOptions,
+  createWrapperOptions,
+  createIconOptions,
+  createCloseButtonOptions,
+  createTransitionOptions
+} from './shared';
 import { BPV_SEVERITY } from '../../../constants';
-import { isNullOrUndefined } from '../../../utils/validators';
+import { inArrayValidator } from '../../../utils/validators';
 
 export const messagePT = <MessagePassThroughOptions>{
-  root: ({ props, ...rest }) => {
-    Object.defineProperty(rest.instance, 'iconComponent', {
-      get() {
-        const obj: Partial<Record<(typeof BPV_SEVERITY)[number], typeof Icon>> = {
-          info: InfoCircleIcon,
-          success: CheckIcon,
-          warning: ExclamationTriangleIcon,
-          danger: TimesCircleIcon
-        };
-
-        obj.primary = obj.secondary = obj.dark = obj.light = obj.info;
-        return obj[(props.severity as (typeof BPV_SEVERITY)[number]) || 'info'];
-      }
-    });
-
-    return {
-      class: ['alert', 'bpv-alert', `alert-${isNullOrUndefined(props.severity) ? 'info' : props.severity}`]
-    };
+  root: ({ props, instance }) => {
+    const severity = inArrayValidator(props.severity, BPV_SEVERITY, 'info');
+    return createRootOptions({ severity, instance });
   },
-  wrapper: () => ({
-    class: 'bpv-alert-wrapper'
-  }),
-  icon: () => {
-    return {
-      class: 'bpv-alert-icon'
-    };
-  },
-  closeButton: () => ({
-    class: 'btn bpv-alert-btn-close'
-  }),
-  transition: {
-    name: 'bpv-alert'
-  }
+  wrapper: () => createWrapperOptions(),
+  icon: () => createIconOptions(),
+  closeButton: () => createCloseButtonOptions(),
+  transition: createTransitionOptions()
 };
---
+++ src/runtime/presets/bootstrap5/message/shared.ts
@@ -0,0 +1,58 @@
+import type { Icon } from 'primevue/icons';
+import type { BPVSeverity } from '../../../types';
+
+import InfoCircleIcon from 'primevue/icons/infocircle';
+import CheckIcon from 'primevue/icons/check';
+import ExclamationTriangleIcon from 'primevue/icons/exclamationtriangle';
+import TimesCircleIcon from 'primevue/icons/timescircle';
+
+export interface CreateRootOptionsOptions {
+  severity: BPVSeverity;
+  instance: any;
+}
+
+export const createRootOptions = (options: CreateRootOptionsOptions) => {
+  const { severity, instance } = options;
+
+  if (!Object.prototype.hasOwnProperty.call(instance, '__definedCustomIconComponents__')) {
+    Object.defineProperty(instance, '__definedCustomIconComponents__', {
+      configurable: false,
+      value: true,
+      enumerable: false,
+      writable: false
+    });
+    Object.defineProperty(instance, 'iconComponent', {
+      get() {
+        const obj: Partial<Record<BPVSeverity, typeof Icon>> = {
+          info: InfoCircleIcon,
+          success: CheckIcon,
+          warning: ExclamationTriangleIcon,
+          danger: TimesCircleIcon
+        };
+
+        obj.primary = obj.secondary = obj.dark = obj.light = obj.info;
+        return obj[severity || 'info'];
+      }
+    });
+  }
+
+  const classes = ['alert', 'bpv-alert', `alert-${severity}`];
+
+  return { class: classes };
+};
+
+export const createWrapperOptions = () => ({
+  class: 'bpv-alert-wrapper'
+});
+
+export const createIconOptions = () => ({
+  class: 'bpv-alert-icon'
+});
+
+export const createCloseButtonOptions = () => ({
+  class: 'btn bpv-alert-btn-close'
+});
+
+export const createTransitionOptions = () => ({
+  name: 'bpv-alert'
+});
