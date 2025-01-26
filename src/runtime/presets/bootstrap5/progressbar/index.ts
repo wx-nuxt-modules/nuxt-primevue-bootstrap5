7fd16ae74f0d1925f82986210298e1f0b63abc6d
windx-foobar <bezalkogoln1ycoder@gmail.com>
2025-01-27 00:39:25 +0300

    fix(bootstrap5/progressbar): allow custom `severity`

--- src/runtime/presets/bootstrap5/progressbar/index.ts
+++ src/runtime/presets/bootstrap5/progressbar/index.ts
@@ -20,14 +20,13 @@ export const progressbarPT: Partial<ProgressBarPassThroughOptions> = {
   }: ProgressBarPassThroughOptionType & {
     attrs: { severity?: string; striped?: boolean | ''; animated?: boolean | '' };
   }) => {
-    const severity = inArrayValidator(attrs.severity, BPV_SEVERITY, 'primary');
     const striped = attrs.striped === '' || attrs.striped === true;
     const animated = attrs.animated === '' || attrs.animated === true;
 
     return {
       class: [
         'progress-bar',
-        `bg-${severity}`,
+        `bg-${attrs.severity || 'primary'}`,
         striped && 'progress-bar-striped',
         striped && animated && 'progress-bar-animated'
       ]
